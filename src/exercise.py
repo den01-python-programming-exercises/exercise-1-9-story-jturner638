021333ad7fbcd3910b38d783fb5e2fc1ca6fd08e
Justin Turner <mrfathed@protonmail.com>
2021-12-03 13:26:32 -0600

    Third Try

--- src/exercise.py
+++ src/exercise.py
@@ -5,7 +5,7 @@ def main():
     job = input("What is their job?")
 
     print("Here is the story:")
-    print("Once upon a time there was {}, who was {}".format(name, job))
+    print("Once upon a time there was {}, who was {}.".format(name, job))
     print("On the way to work, {} reflected on life.".format(name))
     print("Perhaps {} will not be {} forever".format(name, job))
 
